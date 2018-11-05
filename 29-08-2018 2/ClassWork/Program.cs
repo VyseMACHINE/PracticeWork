319e5bb59e7c0496a02bdc505c9386ede54d81ca
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2018-11-05 21:44:13 +0600

    Add files via upload

---
+++ 29-08-2018 2/ClassWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassWork
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int number1, number2;
+			int summary;
+			
+			Console.WriteLine("Введите Первое число: ");
+			number1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Введите Второе число: ");
+			number2 = int.Parse(Console.ReadLine());
+			
+			summary = number1 + number2;
+			
+			Console.WriteLine("Сумма чисел {0} и {1} = {2}", number1, number2, summary);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ 29-08-2018/ClassWork/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alibek
+ * Date: 07.09.2018
+ * Time: 11:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ClassWork
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string name, surname;
+			
+			Console.WriteLine("Введите Ваше имя: ");
+			name = Console.ReadLine();
+			Console.WriteLine("Введите Вашу фамилию: ");
+			surname = Console.ReadLine();
+			
+			Console.WriteLine("Ваше имя и фамилия {0} {1}", name , surname);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
